a0b9ade434fe593ffb3367a64e20ec057baf6a4e
scostadavid <davidshcosta@gmail.com>
2024-06-01 21:43:53 -0300

    fix: contact typo

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -1,5 +1,5 @@
 export const links = [
   {id: 1, title: 'Email', url: 'mailto:scostadavid@proton.me', urlText: 'scostadavid@proton.me'},
   {id: 2, title: 'Linkedin', url: 'https://linkedin.com/in/scostadavid', urlText: 'in/scostadavid'},
-  {id: 3, title: 'Gituhb', url: 'https://github.com/scostadavid', urlText: 'gh/scostadavid'},
+  {id: 3, title: 'Github', url: 'https://github.com/scostadavid', urlText: 'gh/scostadavid'},
 ];
